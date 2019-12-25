64ba5a9e7db2f6315237992bbe2acc1331ced1a0
Tim Hammerquist <penryu@gmail.com>
2019-12-25 02:37:29 -0800

    now 100% more observable

--- async/ts/async.ts
+++ async/ts/async.ts
@@ -1,8 +1,5 @@
 import fetchURL from 'node-fetch';
-import * as path from 'path';
-import * as process from 'process';
-
-const RequestCount: number = 500;
+import { Observable } from 'rxjs';
 
 async function fetchEndpoint(url: string): Promise<number> {
     const resp = await fetchURL(url);
@@ -38,7 +35,36 @@ async function runAsync(url: string, count: number): Promise<string> {
     return `Async: ${bytes} bytes in ${deltaT}ms`;
 }
 
+function runObservable(url: string, count: number): Promise<string> {
+    console.log('Observable starting...');
+
+    return new Promise((resolve) => {
+        const startTime = Date.now();
+        const observableFetches = new Observable((subscriber) => {
+            const promises = [];
+            for (let i = 0; i < count; ++i) {
+                promises.push(fetchEndpoint(url).then(b => subscriber.next(b)));
+            }
+            Promise.all(promises).then(() => subscriber.complete());
+        });
+
+        const results = [];
+        observableFetches.subscribe({
+            next(x) { results.push(x); },
+            complete() {
+                const deltaT = Date.now() - startTime;
+                const bytes = results.reduce((acc, n) => (acc + n), 0);
+                resolve(`Observable ${bytes} bytes in ${deltaT}ms`);
+            }
+        });
+    });
+}
+
 async function main() {
+    const path = require('path');
+    const process = require('process');
+    const RequestCount: number = 50;
+
     if (process.argv.length < 3) {
         const myname = path.basename(process.argv[1]);
         console.log(`Usage: ${myname} URL`);
@@ -47,6 +73,7 @@ async function main() {
     const TargetUrl: string = process.argv[2];
     runSync(TargetUrl, RequestCount).then(console.log);
     runAsync(TargetUrl, RequestCount).then(console.log);
+    runObservable(TargetUrl, RequestCount).then(console.log);
 }
 
 main();
