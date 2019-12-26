14f0362161f768146bff34bc68efee77b37a3ae9
Tim Hammerquist <penryu@gmail.com>
2019-12-26 00:12:28 -0800

    refactor Observables with operators; comment more

--- async/ts/async.ts
+++ async/ts/async.ts
@@ -1,13 +1,17 @@
 import fetchURL from 'node-fetch';
-import { Observable } from 'rxjs';
+import { from, merge, Observable } from 'rxjs';
+import { reduce } from 'rxjs/operators';
 
 async function fetchEndpoint(url: string): Promise<number> {
+    // start the fetch (header and first chunk only)
     const resp = await fetchURL(url);
+    // ensure full text (may be multiple chunks)
     const text = await resp.text();
     return text.length;
 }
 
-async function runSync(url: string, count: number): Promise<number> {
+async function runSync(url: string, count: number) {
+    // invokes async function synchronously {count} times in sequence
     let bytes: number = 0;
     for (let i = 0; i < count; ++i) {
         bytes += await fetchEndpoint(url);
@@ -16,31 +20,32 @@ async function runSync(url: string, count: number): Promise<number> {
 }
 
 async function runAsync(url: string, count: number): Promise<number> {
+    // starts {count} async operations concurrently, collecting their promises
     const promises = [];
     for (let i = 0; i < count; ++i) {
         promises.push(fetchEndpoint(url));
     }
+    // wait for _all_ promises to complete
     const results = await Promise.all(promises);
+    // sum and return
     return results.reduce((acc, n) => (acc + n), 0);
 }
 
 function runObservable(url: string, count: number): Promise<number> {
-    return new Promise((resolve) => {
-        const observableFetches = new Observable((subscriber) => {
-            const promises = [];
-            for (let i = 0; i < count; ++i) {
-                promises.push(fetchEndpoint(url).then(b => subscriber.next(b)));
-            }
-            Promise.all(promises).then(() => subscriber.complete());
-        });
+    // construct {count} observables from the {count} promises
+    // (similar to runAsync)
+    const requests: Array<Observable<number>> = Array(count).fill(url)
+        .map((u) => from(fetchEndpoint(u)));
+    // merge Array<Observable<number>> => Observable<Array<number>>
+    const oBytes: Observable<number> = merge(...requests)
+        // ... => Observable<number> containing sum total bytes
+        .pipe(reduce((acc, x) => acc + x));
 
-        const results = [];
-        observableFetches.subscribe({
-            next(x) { results.push(x); },
-            complete() {
-                const bytes = results.reduce((acc, n) => (acc + n), 0);
-                resolve(bytes);
-            }
+    // return Promise resolved/rejected by the outcome of Observable<number>
+    return new Promise((resolve, reject) => {
+        oBytes.subscribe({
+            next(x) { resolve(x); },
+            error(e) { reject(e); },
         });
     });
 }
@@ -57,17 +62,19 @@ async function main() {
     }
     const TargetUrl: string = process.argv[2];
 
-    const operations = new Map<CallableFunction, string>([
+    // map operation to label for data-driven operation
+    const operationMap: Map<CallableFunction, string> = new Map([
         [runSync, 'Sync'],
         [runAsync, 'Async'],
         [runObservable, 'Observable'],
     ]);
 
-    operations.forEach((label: string, f: CallableFunction) => {
+    operationMap.forEach((label, f) => {
         console.log(`${label} starting...`);
-        const start = Date.now();
+
+        const startTime: number = Date.now();
         f(TargetUrl, RequestCount).then((bytes) => {
-            const delta = Date.now() - start;
+            const delta = Date.now() - startTime;
             console.log(`${label}: ${bytes} bytes in ${delta}ms.`);
         });
     });
