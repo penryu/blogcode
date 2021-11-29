f3b43ac4b6a86d8452dbfe5d6bc25b598b4e8d68
Tim Hammerquist <tim@penryu.dev>
2021-11-29 02:08:47 -0800

    keeping basic impl simple, and adding generics to final

--- topqueue-rs/src/lib.rs
+++ topqueue-rs/src/lib.rs
@@ -3,10 +3,23 @@
 //! This is a Rust port of the code from my series of posts on implementing a
 //! custom Priority Queue, originally in Scala[^original].
 //!
+//! Rust counterparts to some of the naive code samples from [page 1] can be found
+//! in [util].
+//!
+//! A Rust port of the initial implementation of TopQueue is in
+//! [topqueue_basic].
+//!
+//! The final Rust version, featuring most applicable elements from the final
+//! Scala version, is in [topqueue_final].
+//!
 //! [^original]: The original series, "A Scala Interview", can be found [here].
 //!
 //! [here]: https://lex.penryu.dev/posts/scala-interview-1
+//! [page 1]: https://lex.penryu.dev/posts/scala-interview-1
+//! [topqueue_basic]: topqueue_basic/index.html
+//! [topqueue_final]: topqueue_final/index.html
+//! [util]: util/index.html
 
-pub mod page1;
-
-pub mod page2;
+pub mod util;
+pub mod topqueue_basic;
+pub mod topqueue_final;
--- topqueue-rs/src/util.rs
+++ topqueue-rs/src/util.rs
@@ -1,10 +1,7 @@
-//! [`get_top()`] implements a brute force method of selecting the
-//! largest `top` integers from the given collection.
-//!
-//! [`get_top()`]: fn.get_top.html
+//! Utility functions and example code.
 //!
 //! ```
-//! # use topqueue::page1::*;
+//! # use topqueue::util::*;
 //! let n_rands: Vec<i32> = make_rands(10).collect();
 //! let top_m = get_top(&n_rands, 3);
 //!
@@ -17,7 +14,7 @@
 //! Same as above, but MOAR.
 //!
 //! ```
-//! # use topqueue::page1::*;
+//! # use topqueue::util::*;
 //! let n_rands: Vec<i32> = make_rands(1_000_000).collect();
 //! let top_m = get_top(&n_rands, 100);
 //!
@@ -30,7 +27,7 @@
 //! Same method on a rediciously inefficient scale!
 //!
 //! ```no_run
-//! # use topqueue::page1::*;
+//! # use topqueue::util::*;
 //! let n_rands: Vec<i32> = make_rands(100_000_000).collect();
 //! let top_m = get_top(&n_rands, 100);
 //!
@@ -39,10 +36,6 @@
 //!     top_m.iter().max()
 //! );
 //! ```
-//!
-//! This file correspond to example code in the first [blog post].
-//!
-//! [blog post]: https://lex.penryu.dev/posts/scala-interview-1
 
 use std::iter::from_fn;
 use rand::{Rng, thread_rng};
@@ -52,9 +45,10 @@ pub fn make_rands(len: usize) -> impl Iterator<Item=i32> {
     from_fn(move || Some(thread_rng().gen())).take(len)
 }
 
-/// Takes a vector `vec` and returns the `top` largest values.
+/// `get_top()` implements a brute force method of selecting the
+/// largest `top` integers from the given vector.
 pub fn get_top(vec: &Vec<i32>, top: usize) -> Vec<i32> {
-    let mut dupe = vec.clone().into_iter().collect::<Vec<i32>>();
+    let mut dupe = vec.to_owned().into_iter().collect::<Vec<i32>>();
     dupe.sort_unstable_by(|a, b| b.cmp(a));
     dupe.into_iter().take(top).collect()
 }
